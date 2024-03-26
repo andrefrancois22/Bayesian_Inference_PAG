10ed91b831e63478971f718099960ec2fbcd438d
andrefrancois22 <thomas.langlois@berkeley.edu>
2024-03-26 14:57:42 -0400

    adjusted initial offset histograms and added simulation of dynamic range

--- analysis/F3.m
+++ analysis/F3.m
@@ -182,10 +182,10 @@ for iSfln = 1:29
         
         % ==> store all single trial dv (model fit) initial offsets 
         % normalized by average dynamic range
-        vcwlosF  = [vcwlosF;  vcwlo(:,1)];%  / dynr_mu ];
-        vccwlosF = [vccwlosF; vccwlo(:,1)];% / dynr_mu ]; 
-        vcwhisF  = [vcwhisF;  vcwhi(:,1)];%  / dynr_mu ];
-        vccwhisF = [vccwhisF; vccwhi(:,1)];% / dynr_mu ];      
+        vcwlosF  = [vcwlosF;  vcwlo];%  / dynr_mu ];
+        vccwlosF = [vccwlosF; vccwlo];% / dynr_mu ]; 
+        vcwhisF  = [vcwhisF;  vcwhi];%  / dynr_mu ];
+        vccwhisF = [vccwhisF; vccwhi];% / dynr_mu ];      
         
     % => if Monkey J    
     elseif iSfln >= 14
@@ -213,10 +213,10 @@ for iSfln = 1:29
         
         % ==> store all single trial dv (model fit) initial offsets 
         % normalized by average dynamic range
-        vcwlosJ  = [vcwlosJ;  vcwlo(:,1)];%  / dynr_mu ];
-        vccwlosJ = [vccwlosJ; vccwlo(:,1)];% / dynr_mu ]; 
-        vcwhisJ  = [vcwhisJ;  vcwhi(:,1)];%  / dynr_mu ];
-        vccwhisJ = [vccwhisJ; vccwhi(:,1)];% / dynr_mu ];      
+        vcwlosJ  = [vcwlosJ;  vcwlo];%  / dynr_mu ];
+        vccwlosJ = [vccwlosJ; vccwlo];% / dynr_mu ]; 
+        vcwhisJ  = [vcwhisJ;  vcwhi];%  / dynr_mu ];
+        vccwhisJ = [vccwhisJ; vccwhi];% / dynr_mu ];      
         
     end    
 end
@@ -225,28 +225,43 @@ end
 close all; clc;
 
 % ==> all CW offset values for vertical stimulus - across hi and lo contrasts
-cwsF  = [vcwlosF; vcwhisF];   %cwsF  = cwsF(:,1);
+cwsF  = [vcwlosF; vcwhisF];   
+% ==> initial offset
+cwsFi  = cwsF(:,1);
 % ==> all CCW offset values for vertical stimulus - across hi and lo contrasts
-ccwsF = [vccwlosF; vccwhisF]; %ccwsF = ccwsF(:,1);
+ccwsF = [vccwlosF; vccwhisF]; 
+% ==> initial offset
+ccwsFi = ccwsF(:,1);
 
 % ==> all CW offset values for vertical stimulus - across hi and lo contrasts
-cwsJ  = [vcwlosJ; vcwhisJ];   %cwsJ  = cwsJ(:,1);
+cwsJ  = [vcwlosJ; vcwhisJ];   
+% ==> initial offset
+cwsJi  = cwsJ(:,1);
 % ==> all CCW offset values for vertical stimulus - across hi and lo contrasts
-ccwsJ = [vccwlosJ; vccwhisJ]; %ccwsJ = ccwsJ(:,1);
+ccwsJ = [vccwlosJ; vccwhisJ]; 
+% ==> initial offset
+ccwsJi = ccwsJ(:,1);
+
+% ==>
+cwsF_mu  = mean(cwsF(:,il2:iu2),2);
+ccwsF_mu = mean(ccwsF(:,il2:iu2),2);
+
+cwsJ_mu  = mean(cwsJ(:,il2:iu2),2);
+ccwsJ_mu = mean(ccwsJ(:,il2:iu2),2);
 
 % ==> edges
 edgs = -1.5:0.1:1.5;
 
 fg = figure(); set(fg,'color','white'); set(fg, 'Position',[675 421 660 541])
 subplot(2,2,1);
-hFcw = histogram(cwsF,edgs,'facecolor',[0.15,0.75,0.5]); hold on; hold all;
+hFcw = histogram(cwsF_mu,edgs,'facecolor',[0.15,0.75,0.5]); hold on; hold all;
 plot(zeros(1,max(hFcw.Values)),1:max(hFcw.Values),'r--','linewidth',2)
 title('Animal F: cw prior context')
 xlabel('Signed DV initial offset')
 ylabel('Frequency')
 drawnow;
 subplot(2,2,2);
-hFccw = histogram(ccwsF,edgs,'facecolor',[0.15,0.75,0.5]); hold on; hold all;
+hFccw = histogram(ccwsF_mu,edgs,'facecolor',[0.15,0.75,0.5]); hold on; hold all;
 plot(zeros(1,max(hFccw.Values)),1:max(hFccw.Values),'r--','linewidth',2)
 title('Animal F: ccw prior context')
 xlabel('Signed DV initial offset')
@@ -254,14 +269,14 @@ ylabel('Frequency')
 drawnow;
  
 subplot(2,2,3);
-hJcw = histogram(cwsJ,edgs,'facecolor',[1,0.5,0]); hold on; hold all;
+hJcw = histogram(cwsJ_mu,edgs,'facecolor',[1,0.5,0]); hold on; hold all;
 plot(zeros(1,max(hJcw.Values)),1:max(hJcw.Values),'r--','linewidth',2)
 title('Animal J: cw prior context')
 xlabel('Signed DV initial offset')
 ylabel('Frequency')
 drawnow;
 subplot(2,2,4);
-hJccw = histogram(ccwsJ,edgs,'facecolor',[1,0.5,0]); hold on; hold all;
+hJccw = histogram(ccwsJ_mu,edgs,'facecolor',[1,0.5,0]); hold on; hold all;
 plot(zeros(1,max(hJccw.Values)),1:max(hJccw.Values),'r--','linewidth',2)
 title('Animal J: ccw prior context')
 xlabel('Signed DV initial offset')
@@ -270,11 +285,11 @@ drawnow;
 
 % ==> 
 % ==> signrank wilcoxon test
-[p_cwJ,~]  = signrank(cwsJ);
-[p_ccwJ,~] = signrank(ccwsJ);
+[p_cwJ,~]  = signrank(cwsJ_mu);
+[p_ccwJ,~] = signrank(ccwsJ_mu);
 
-[p_cwF,~]  = signrank(cwsF);
-[p_ccwF,~] = signrank(ccwsF);
+[p_cwF,~]  = signrank(cwsF_mu);
+[p_ccwF,~] = signrank(ccwsF_mu);
 
 fprintf('Wilcoxon test p values evaluating initial offsets in the vertical stimulus condition...\n')
 fprintf('Wilcoxon test for animal F cw cases, p = %d...\n', p_cwF)
@@ -285,9 +300,9 @@ fprintf('Wilcoxon test for animal J cw cases, p = %d...\n', p_cwJ)
 fprintf('Wilcoxon test for animal J ccw cases, p = %d...\n', p_ccwJ)
  
 % ==> ranksum
-[PJ,~] = ranksum(cwsJ,ccwsJ)
+[PJ,~] = ranksum(cwsJ_mu,ccwsJ_mu)
 
-[PF,~] = ranksum(cwsF,ccwsF)
+[PF,~] = ranksum(cwsF_mu,ccwsF_mu)
 
 % ==> restrict to the low contrast trials and repeat ranksum
 
---
+++ analysis/F4.m
@@ -0,0 +1,59 @@
+clear all; close all; clc;
+% ==> directories
+dataPath     = strcat('/home/thomas/Desktop/UTAustin/Goris/pfc_code/pfc_data');
+drc = '../data/';
+
+%%
+
+% ==> what is the session
+for iSfln = 17%:29 
+
+    % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    % params: matrix with the model parameters [trial x parameter]
+    params = load([drc,'trial_DV_params_iS_',num2str(iSfln),'.mat']);
+    params = params.ps_cat;
+    fprintf('Finished loading matrix with the model parameters for iS = %d... \n',iSfln)
+    % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    % dvs are model predicted DV trajectories [trial x time] (Category DVs)
+    dvs = load([drc,'trial_DV_traj_iS_',num2str(iSfln),'.mat']);
+    dvs = dvs.dv_cat;
+    fprintf('Finished loading model predicted DV trajectories (Categorical DVs) for iS = %d... \n',iSfln)
+    % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    % ==> load session data
+    if iSfln > 9
+        load([dataPath,'/dataSet_',num2str(iSfln),'.mat']);
+    elseif iSfln <= 9
+        load([dataPath,'/dataSet_0',num2str(iSfln),'.mat']);
+    end
+    timeSac = S.dec.timeSac;
+    % Set time boundaries
+    fitTimeSacBegin = -800;  % in ms, relative to saccade onset
+    fitTimeSacEnd   = -50;
+    [~, sacBegin] = min(abs(timeSac - fitTimeSacBegin));
+    [~, sacEnd]   = min(abs(timeSac - fitTimeSacEnd));
+
+    % time intervals
+    t = linspace(timeSac(sacBegin),timeSac(sacEnd),size(dvs,2));
+
+    % ==> choice
+    cho = S.beh.choiceCat;
+    % ==> context, contrast, orientation indicator variables
+    ctx = S.exp.taskContext; ctr = S.exp.stimContrast; ori = S.exp.stimOriDeg;
+    % x axis is orientation (the values differ for FN and JP!). Use unique values
+    or = unique(ori)'; cx = unique(ctx)'; cr = unique(ctr)';
+    
+    % ==> dynamic range for session
+    dynr = max([max(dvs, [], 2) - dvs(:,1), -(min(dvs, [], 2) - dvs(:,1))], [], 2);
+    
+end
+
+%% ==> dynamic range median split
+
+%% ==> PF curve fit
+
+%% ==> delta bias and delta uncertainty computations
+
+%% ==> choice predictivity and AIC analysis
