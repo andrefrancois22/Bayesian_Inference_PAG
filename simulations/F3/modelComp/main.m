e6545273907ff5d7b12cf5d992bbbf36b4cd95e6
andrefrancois22 <thomas.langlois@berkeley.edu>
2024-06-30 14:42:03 -0500

    tested new optimization algorithm on more session data, with good results

--- simulations/F3/modelComp/main.m
+++ simulations/F3/modelComp/main.m
@@ -15,15 +15,6 @@ load([drc,'CTs_dynr_',dyn_type,'.mat'],'CTs');
 % PFs (<Session ID>_<contrast>_<dynamic range split>)
 load([drc,'PFs_dynr_',dyn_type,'.mat'],'PF');
 
-% ==> parameters 
-% => number of simulated trials
-N = 500;
-tm = 200; % timepoints (same resolution as actual DV fits)  
-% ==> bound
-bnd = 12; 
-% ==> sd (for diffusion component)
-sd = 1;
-
 % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 % ==> PF curve estimation Set options
 fitFlag         = 1;       % 0 or 1, where 1 means do the fit right now
@@ -41,150 +32,208 @@ LB(4,1)  = 0;                         UB(4,1) = 200;   % initial offset
 % LB(5,1)  = 6;                         UB(5,1) = 20;   % bound
 % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+% ==> fit or used cashed results?
+% FIT_FLAG = false;
+FIT_FLAG = true;
+
 % ==> for each session, and for each contrast level, fit acc-bound predicted dynr PFs curves to actual data
-iS = 17;
-cr = 2; % => lo contrast (fit models separately for different stimulus contrast images)
+iS = 1;
+cr = 1; % => lo contrast (fit models separately for different stimulus contrast images)
+
+% ==> parameters for accumulation to bound forward model
+% => number of simulated trials (forward model is pretty fast)
+N = 500;
+tm = 200; % timepoints (same resolution as actual DV fits)  
+% ==> bound
+bnd = 12; 
+% ==> sd (for diffusion component)
+sd = 1;
+
+% ==> grid resolution for coarse initial parameter setting grid search (4D) - step
+stp = 10;
+% ==> repeats for fmincon (~'simulated annealing' iterative procedure)
+r = 100;
 % ==> accumulation to bound model type
 mod_type = 'm2';
 
-% ==> fmincon optim often converges to bad local minima - sensitive to
-% initial parameter settings. Initialize with best results from a coarse
-% grid search
+% ==> check for cashed results
+try
+    ps = load(['params/','iS_',num2str(iS),'_cr_',num2str(cr),'_startVec.mat']);
+    % ==> don't fit if you have results (could change this later)
+    FIT_FLAG = false;
+catch
+    % ==> command line warning
+    fprintf('no saved results for session %d for contrast %d, Running optimization...\n', iS, cr);
+end
 
-% ==> grid resolution - step
-stp = 10;
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ %
+% ~~~~~~~~~~~~~~~~~~~~~~~ optimization procedure ~~~~~~~~~~~~~~~~~~~~~~~~ %
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ %
+% ==> fmincon optim often converges to bad local minima - sensitive to    %
+% initial parameter settings. Initialize with best results from a coarse  %
+% grid search, then rerun fmincon with a simulated annealing kind of      %
+% procedure - e.g. rerun fmincon with its optimal results from the        %     
+% previous iteration if a lower NLL is achieved. This seems to help a lot %
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ %
 
-itsvs = linspace(LB(1,1),UB(1,1),stp); % --> orientation mus interval
-sftvs = linspace(LB(2,1),UB(2,1),stp); % --> horizontal shift
-dftvs = linspace(LB(3,1),UB(3,1),stp); % --> drift rate
-oftvs = linspace(LB(4,1),UB(4,1),stp); % --> initial offset
+if FIT_FLAG
+    itsvs = linspace(LB(1,1),UB(1,1),stp); % --> orientation mus interval
+    sftvs = linspace(LB(2,1),UB(2,1),stp); % --> horizontal shift
+    dftvs = linspace(LB(3,1),UB(3,1),stp); % --> drift rate
+    oftvs = linspace(LB(4,1),UB(4,1),stp); % --> initial offset
 
-% ==> forward model NNLs
-NNLs_init = nan(stp,stp,stp,stp);
-
-% ==> brute force grid search for initial param settings before fmincon
-i = 1;
-for itvi = 1:stp                                    % --> orientation mus interval
-    % => interval
-    itv = itsvs(itvi);
-    for sfti = 1:stp                                % --> horizontal shift
-        % => shift
-        sft = sftvs(sfti);
-        for dfti = 1:stp                            % --> drift rate
-            % => drift rate
-            dft = dftvs(dfti);
-            for ofti = 1:stp                        % --> initial offset
-                % => offset
-                oft = oftvs(ofti);               
-                % ==> interval, overall bias (shift), fc (drift rate), initial offset
-                startVec = [itv, sft, dft, oft];                
-                % ==> run forward model    
-                [nll, ~, ~] = modfitf(iS, cr, CTs, N, tm, sd, startVec, mod_type);
-                % ==> store
-                NNLs_init(itvi,sfti,dfti,ofti) = nll;
-                % => for command line updates
-                i = i + 1;
-            end            
+    % ==> forward model NNLs
+    NNLs_init = nan(stp,stp,stp,stp);
+    
+    % ==> this takes a few minutes
+    fprintf('Running initial parameter coarse grid search before fmincon (takes minutes)...\n');
+    
+    % ==> brute force grid search for initial param settings before fmincon
+    i = 1;
+    for itvi = 1:stp                                    % --> orientation mus interval
+        % => interval
+        itv = itsvs(itvi);
+        for sfti = 1:stp                                % --> horizontal shift
+            % => shift
+            sft = sftvs(sfti);
+            for dfti = 1:stp                            % --> drift rate
+                % => drift rate
+                dft = dftvs(dfti);
+                for ofti = 1:stp                        % --> initial offset
+                    % => offset
+                    oft = oftvs(ofti);               
+                    % ==> interval, overall bias (shift), fc (drift rate), initial offset
+                    startVec = [itv, sft, dft, oft];                
+                    % ==> run forward model    
+                    [nll, ~, ~] = modfitf(iS, cr, CTs, N, tm, sd, startVec, mod_type);
+                    % ==> store
+                    NNLs_init(itvi,sfti,dfti,ofti) = nll;
+                    % => for command line updates
+                    i = i + 1;
+                end            
+            end
         end
+        % => update               
+        clc; fprintf('completed step %d of %d...\n',i,stp^length(LB));  
     end
-    % => update               
-    clc; fprintf('completed step %d of %d...\n',i,stp^length(LB));  
+
+    % ==> return 4 indices of min NLL in NNLs_init
+    [min_nll, idx] = min(NNLs_init(:));
+    [i,j,k,l] = ind2sub( size(NNLs_init), idx );
+
+    % ==> could visualize 4 x 4 heatmaps of pairwise NLL maps for params
+    
+    % ==> interval, overall bias (shift), fc (drift rate), initial offset, bound
+    startVec = [itsvs(i),sftvs(j),dftvs(k),oftvs(l)]; %[0.038, 0, 8, 90];%, 12]; 
+
+    % ==> NLLs
+    nlls = cell(r,1);
+    % ==> simulation optim params
+    startVecs = cell(r,1);
+
+    % ==> objective function for fmincon
+    obFun = @(paramVec) modfitf(iS, cr, CTs, N, tm, sd, paramVec, mod_type); 
+
+    % ==> run fmincon several times (variable results even with multistart)
+    for rep = 1:r
+        % ==> run optimization
+        simfit = fmincon(obFun, startVec, [], [], [], [], LB, UB, [], options);
+        % ==> return best fit simulated dynamic range proportions
+        [nll, ~, ~] = modfitf(iS, cr, CTs, N, tm, sd, simfit, mod_type);
+        % ==> track nlls
+        nlls{rep} = nll;    
+        % ==> reset startVec if new minimum for nll in next round
+        % ==> kind of a simulated annealing procedure...
+        if nll <= min(cell2mat(nlls))
+            % ==> startVec
+            startVec = simfit;
+            % ==> store
+            startVecs{rep} = simfit;
+        end    
+        fprintf('Finished simulated annealing step %d of %d...\n',rep,r);
+    end
+
+    % ==> find index of min
+    [~,I] = min(cell2mat(nlls));
+    % ==> set optimal paramters
+    simfit = startVecs{I};
+    % ==> run forward model
+    [nll, propcw, propccw] = modfitf(iS, cr, CTs, N, tm, sd, simfit, mod_type);
+    % ==> update
+    fprintf('Done with artificial multistart...\n')
+
+    % ==> cashe the results
+    save(['params/','iS_',num2str(iS),'_cr_',num2str(cr),'_startVec.mat'],'simfit','propcw','propccw','nll');
+    % => FIT_FLAG
+    FIT_FLAG = false;
 end
+    
+if ~FIT_FLAG
+    % ==> inspect results of the optimization
 
-% ==> return 4 indices of min NLL in NNLs_init
-[min_nll, idx] = min(NNLs_init(:));
-[i,j,k,l] = ind2sub( size(NNLs_init), idx );
+    % ==> load optimal parameters and simulated choice proportions
+    ps = load(['params/','iS_',num2str(iS),'_cr_',num2str(cr),'_startVec.mat']);
+    propcw  = ps.propcw;
+    propccw = ps.propccw;
+    simfit  = ps.simfit; 
 
-% ==> interval, overall bias (shift), fc (drift rate), initial offset, bound
-startVec = [itsvs(i),sftvs(j),dftvs(k),oftvs(l)]; %[0.038, 0, 8, 90];%, 12]; 
+    % ==> visualize the results
+    close all;  clc;
+    figure(1); set(gcf,'color','white'); set(gcf,'Position', [33 585 1854 365]);
+    subplot(1,4,1);
+    hold on; hold all;
+    plot(-3:3, propcw(1,:), 'color', [1,0.75,0.75], 'linewidth', 2, 'linestyle', '--');
+    plot(-3:3, propccw(1,:),'color', [0.75,0.75,1], 'linewidth', 2, 'linestyle', '--');
+    plot(-3:3, propcw(2,:), 'r-', 'linewidth', 1, 'linestyle', '--');
+    plot(-3:3, propccw(2,:),'b-', 'linewidth', 1, 'linestyle', '--');
+    xlabel('Orientation');
+    ylabel('Simulated p(cw)');
+    title('Optimized Drift-Diffusion');
+    axis square;
 
-% ==> repeats
-r = 100;
+    subplot(1,4,2);
+    hold on; hold all;
+    plot(-3:3, CPs{iS,cr,2}(2,:), 'b-', 'linewidth', 1)
+    plot(-3:3, CPs{iS,cr,2}(1,:), 'r-', 'linewidth', 1)
+    plot(-3:3, CPs{iS,cr,1}(1,:), 'color', [1,0.75,0.75], 'linewidth', 2)
+    plot(-3:3, CPs{iS,cr,1}(2,:), 'color', [0.75,0.75,1], 'linewidth', 2)
+    xlabel('Orientation');
+    ylabel('Simulated p(cw)');
+    title(['Session ', num2str(iS)]);
+    axis square;
 
-% ==> NLLs
-nlls = cell(r,1);
-% ==> simulation optim params
-startVecs = cell(r,1);
+    subplot(1,4,3);
+    hold on; hold all;
+    plot(-3:3, CTs{iS,cr,1,2}(2,:) ./ (CTs{iS,cr,1,2}(2,:) + CTs{iS,cr,1,1}(2,:)), 'color', [0.75,0.75,1], 'linewidth', 2);
+    plot(-3:3, CTs{iS,cr,1,2}(1,:) ./ (CTs{iS,cr,1,2}(1,:) + CTs{iS,cr,1,1}(1,:)), 'color', [1,0.75,0.75], 'linewidth', 2);
+    plot(-3:3, CTs{iS,cr,2,2}(2,:) ./ (CTs{iS,cr,2,2}(2,:) + CTs{iS,cr,2,1}(2,:)),'color', 'b', 'linewidth', 1);
+    plot(-3:3, CTs{iS,cr,2,2}(1,:) ./ (CTs{iS,cr,2,2}(1,:) + CTs{iS,cr,2,1}(1,:)),'color', 'r', 'linewidth', 1);
+    hold on; hold all;
+    plot(-3:3, propcw(1,:), 'color', [1,0.75,0.75], 'linewidth', 2, 'linestyle', '--');
+    plot(-3:3, propccw(1,:),'color', [0.75,0.75,1], 'linewidth', 2, 'linestyle', '--');
+    plot(-3:3, propcw(2,:), 'r-', 'linewidth', 1, 'linestyle', '--');
+    plot(-3:3, propccw(2,:),'b-', 'linewidth', 1, 'linestyle', '--');
+    xlabel('Orientation');
+    ylabel('Simulated p(cw)');
+    title('Overlay');
+    axis square;
 
-% ==> objective function for fmincon
-obFun = @(paramVec) modfitf(iS, cr, CTs, N, tm, sd, paramVec, mod_type); 
-
-% ==> run fmincon several times (variable results even with multistart)
-for rep = 1:r
-    % ==> run optimization
-    simfit = fmincon(obFun, startVec, [], [], [], [], LB, UB, [], options);
-    % ==> return best fit simulated dynamic range proportions
-    [nll, ~, ~] = modfitf(iS, cr, CTs, N, tm, sd, simfit, mod_type);
-    % ==> track nlls
-    nlls{rep} = nll;    
-    % ==> reset startVec if new minimum for nll in next round
-    % ==> kind of a simulated annealing procedure...
-    if nll <= min(cell2mat(nlls))
-        % ==> startVec
-        startVec = simfit;
-        % ==> store
-        startVecs{rep} = simfit;
-    end    
-    fprintf('Finished simulated annealing step %d of %d...\n',rep,r);
+    subplot(1,4,4);
+    hold  on; hold all;
+    plot(-3:3,propcw(2,:),     'r', 'linewidth', 1, 'linestyle', '--');
+    plot(-3:3,propccw(2,:),    'b', 'linewidth', 1, 'linestyle', '--');
+    plot(-3:3,PF{iS,cr,2}(2,:),'b-', 'linewidth', 1);
+    plot(-3:3,PF{iS,cr,2}(1,:),'r-', 'linewidth', 1);
+    hold on; hold all;
+    plot(-3:3,propcw(1,:),      'color', [1,0.75,0.75], 'linewidth', 2, 'linestyle', '--');
+    plot(-3:3,propccw(1,:),     'color', [0.75,0.75,1], 'linewidth', 2, 'linestyle', '--');
+    plot(-3:3,PF{iS,cr,1}(2,:), 'color', [0.75,0.75,1], 'linewidth', 2);
+    plot(-3:3,PF{iS,cr,1}(1,:), 'color', [1,0.75,0.75], 'linewidth', 2);
+    xlabel('Orientation');
+    ylabel('Simulated p(cw)');
+    title(['Session ', num2str(iS),' PFs']);
+    axis square;
+    
+    ps
 end
-
-% ==> find index of min
-[~,I] = min(cell2mat(nlls));
-% ==> set optimal paramters
-simfit = startVecs{I};
-% ==> run forward model
-[nll, propcw, propccw] = modfitf(iS, cr, CTs, N, tm, sd, simfit, mod_type);
-% ==> update
-fprintf('Done with artificial multistart...\n')
-
-% ==> cashe the results
-save(['params/','iS_',num2str(iS),'_cr_',num2str(cr),'_startVec.mat'],'simfit','propcw','propccw','nll');
-
-% ==> load optimal parameters and simulated choice proportions
-ps = load(['params/','iS_',num2str(iS),'_cr_',num2str(cr),'_startVec.mat']);
-propcw  = ps.propcw;
-propccw = ps.propccw;
-simfit  = ps.simfit; 
-
-% ==> visualize the results
-close all; 
-figure(1);
-subplot(2,3,1);
-hold on; hold all;
-plot(1:7,propcw(1,:),'r-h')
-plot(1:7,propccw(1,:),'b-h')
-plot(1:7,propcw(2,:),'rh:')
-plot(1:7,propccw(2,:),'bh:')
-subplot(2,3,2);
-hold on; hold all;
-plot(CPs{iS,cr,2}(2,:),'b:')
-plot(CPs{iS,cr,2}(1,:),'r:')
-plot(CPs{iS,cr,1}(1,:),'r')
-plot(CPs{iS,cr,1}(2,:),'b')
-subplot(2,3,3);
-hold on; hold all;
-plot(1:7, CTs{iS,cr,1,2}(2,:) ./ (CTs{iS,cr,1,2}(2,:) + CTs{iS,cr,1,1}(2,:)),'b');
-plot(1:7, CTs{iS,cr,1,2}(1,:) ./ (CTs{iS,cr,1,2}(1,:) + CTs{iS,cr,1,1}(1,:)),'r');
-plot(1:7, CTs{iS,cr,2,2}(2,:) ./ (CTs{iS,cr,2,2}(2,:) + CTs{iS,cr,2,1}(2,:)),'b:');
-plot(1:7, CTs{iS,cr,2,2}(1,:) ./ (CTs{iS,cr,2,2}(1,:) + CTs{iS,cr,2,1}(1,:)),'r:');
-hold on; hold all;
-plot(1:7,propcw(1,:), 'r-h')
-plot(1:7,propccw(1,:),'b-h')
-plot(1:7,propcw(2,:), 'rh:')
-plot(1:7,propccw(2,:),'bh:')
-
-
-subplot(2,3,5);
-hold  on; hold all;
-plot(1:7,propcw(2,:), 'rh:')
-plot(1:7,propccw(2,:),'bh:')
-plot(1:7,PF{iS,cr,2}(2,:),'b-')
-plot(1:7,PF{iS,cr,2}(1,:),'r-')
-subplot(2,3,6);
-hold on; hold all;
-plot(1:7,propcw(1,:), 'r-h')
-plot(1:7,propccw(1,:),'b-h')
-plot(1:7,PF{iS,cr,1}(2,:),'b-')
-plot(1:7,PF{iS,cr,1}(1,:),'r-')
-
 
