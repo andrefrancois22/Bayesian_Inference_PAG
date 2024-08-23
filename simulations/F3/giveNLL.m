65d3f5b4d1ae3de6ec6d38ec8a53d4a740d1a877
andrefrancois22 <thomas.langlois@berkeley.edu>
2024-08-23 14:10:48 -0700

    automate figure generation for DDM

---
+++ simulations/F3/giveNLL.m
@@ -0,0 +1,21 @@
+function [NLL, propCW] = giveNLL(params, oriVals, nCCW, nCW, model_type)
+
+for iPF = 1:2   % There are two psychometric functions to be fit
+    
+    % Decode function arguments
+    if model_type(2) == '1'
+        lapseRate = params(iPF);
+        noiseInt  = params(3);
+        decCrit   = params(3+iPF);
+    elseif model_type(2) == '2'
+        lapseRate = params(iPF);
+        noiseInt  = params(2+iPF);
+        decCrit   = params(4+iPF);
+    end
+    propCW(iPF,:) = lapseRate + (1 - 2*lapseRate)*normcdf((oriVals - decCrit)/(2*noiseInt));
+    % NLL(iPF)      = -sum(log(binopdf(nCW(iPF,:), nCCW(iPF,:) + nCW(iPF,:), propCW(iPF,:))));
+    NLL(iPF) = -sum(log(max(1e-300, binopdf(nCW(iPF,:), nCCW(iPF,:) + nCW(iPF,:), propCW(iPF,:)))));
+end
+% ==> sum NLL
+NLL = sum(NLL);
+end
--- simulations/F3/main.m
+++ simulations/F3/main.m
@@ -1,5 +1,7 @@
 clear all; close all; clc;
-rng(1,"twister");
+% rng(1,"twister");
+% ==> Change to a local directory
+FIGDR = 'Figures/';
 
 % ==> parameters ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -13,12 +15,13 @@ t = linspace(-795,-45,200);
 [~,iu2] = min(abs(t + 600)); %-600
 
 % => number of simulated trials
-N = 2000;
+N = 3000;
 tm = 200; % timepoints (same resolution as actual DV fits)  
 % ==> bound
-bnd = 8; %12; -6
+bnd = 8; %8
+
 % ==> initial offset
-ofs = 0.6; %0.25; 
+ofs = 0.6; %0.6; 
 
 % ==> accumulation bound or no bound?
 M_FLAG = 'BOUND'; 
@@ -33,7 +36,7 @@ P_FLAG = 'REG_DRIFT_PRIOR';
 N_FLAG = 'TRIAL_NOISE';
 % ==> define log normal noise
 m = 1; % mean
-v = 10; % variance
+v = 1; % variance
 
 % ==> check
 
@@ -56,7 +59,7 @@ v = 10; % variance
 % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 % ==> standard deviation for randn
-sd = 1;
+sd = 1.25;
 % ==> mean for randn
 intv = 0.05;
 mus = [-3*intv, -2*intv, -intv, 0, intv, 2*intv, 3*intv];
@@ -69,10 +72,13 @@ fcs = 0.0:0.02:0.4; %0:0.01:0.2;
 % => compute simulated dynamic range split, and PFs
 [predPF_ddm, db, dp, prop_cw, prop_ccw, dvs_c_cw, dvs_i_cw, dvs_c_ccw, dvs_i_ccw, csensb_cw_ds, csensb_ccw_ds] = accevbnd(N, tm, bnd, sd, mus, fcs, ofs, M_FLAG, P_FLAG, N_FLAG, m, v);
 
+% ==> plot props or plot pfs?
+plot_data = 'plot_props'; 
+% plot_data = 'plot_pfs';
 
 % ==> plot simulated choice proportions by dynamic range split
 for fci = 3:length(fcs)
-    figure(3); set(gcf,'color','white'); set(gcf, 'Position',[273 218 1150 719]);
+    fig3 = figure(3); set(gcf,'color','white'); set(gcf, 'Position',[273 218 1150 719]);
     subplot(4,7,fci)
     hold on; hold all; 
     plot(mus,squeeze(prop_cw(fci,3,:))', 'b-', 'linewidth', 1.5); 
@@ -84,18 +90,22 @@ for fci = 3:length(fcs)
     axis square;
     drawnow;      
     % ==> plot simulated choice proportions by dynamic range split
-    figure(4); set(gcf,'color','white'); set(gcf, 'Position',[273 218 1150 719]);
+    fig4 = figure(4); set(gcf,'color','white'); set(gcf, 'Position',[273 218 1150 719]);
     subplot(4,7,fci)
     hold on; hold all; 
-    plot(mus,squeeze(prop_cw(fci,1,:))', 'color', [0.5,0.5,1], 'linewidth', 1.5); 
-    plot(mus,squeeze(prop_ccw(fci,1,:))','color', [1,0.5,0.5],'linewidth', 1.5);  
-    plot(mus,squeeze(prop_cw(fci,2,:))', 'b-', 'linewidth', 1.5); 
-    plot(mus,squeeze(prop_ccw(fci,2,:))','r-', 'linewidth', 1.5); 
+    if strcmp(plot_data,'plot_props')
+        plot(mus,squeeze(prop_cw(fci,1,:))', 'color', [0.5,0.5,1], 'linewidth', 1.5); 
+        plot(mus,squeeze(prop_ccw(fci,1,:))','color', [1,0.5,0.5],'linewidth', 1.5);  
+        plot(mus,squeeze(prop_cw(fci,2,:))', 'b-', 'linewidth', 1.5); 
+        plot(mus,squeeze(prop_ccw(fci,2,:))','r-', 'linewidth', 1.5); 
+    end
     % ==> plot PFs over these
-    plot(mus,predPF_ddm{fci,1}(1,:),'bo','LineWidth',1)
-    plot(mus,predPF_ddm{fci,1}(2,:),'ro','LineWidth',1)
-    plot(mus,predPF_ddm{fci,2}(1,:),'color', [0.5,0.5,1],'marker','o','LineWidth',1)
-    plot(mus,predPF_ddm{fci,2}(2,:),'color', [1,0.5,0.5],'marker','o','LineWidth',1)    
+    if strcmp(plot_data,'plot_pfs')
+        plot(mus,predPF_ddm{fci,1}(1,:),'b-','LineWidth',1.5)
+        plot(mus,predPF_ddm{fci,1}(2,:),'r-','LineWidth',1.5)
+        plot(mus,predPF_ddm{fci,2}(1,:),'color', [0.5,0.5,1],'LineWidth',1.5)
+        plot(mus,predPF_ddm{fci,2}(2,:),'color', [1,0.5,0.5],'LineWidth',1.5)   
+    end
     xlabel('Orientation');
     ylabel('p(cw)')
     %xlim([-max(mus),max(mus)]); 
@@ -103,6 +113,42 @@ for fci = 3:length(fcs)
     axis square;
     drawnow;  
 end
+% ==> print figure to svg
+print(fig4,[FIGDR,'SI-DYN_RANGE_PROPS', '-', P_FLAG, '-', M_FLAG, '-', N_FLAG, '-SD-', num2str(sd),'.svg'],'-dsvg')
+% ==> print figure to svg
+print(fig3,[FIGDR,'SI-SIMPLE_PROPS', '-', P_FLAG, '-', M_FLAG, '-', N_FLAG, '-SD-', num2str(sd),'.svg'],'-dsvg')
+
+% ==> nice sanity check - plot all curves in same subpanels
+% ==> plot PFs over these
+for fci = 3:length(fcs)
+    fig5 = figure(5); set(gcf,'color','white'); set(gcf, 'Position',[273 218 1150 719]);
+    subplot(4,7,fci)
+    hold on; hold all; 
+    % plot(mus,squeeze(prop_cw(fci,3,:))', 'b-', 'linewidth', 1.5); 
+    % plot(mus,squeeze(prop_ccw(fci,3,:))','r-', 'linewidth', 1.5); 
+    % xlabel('Orientation');
+    % ylabel('p(cw)')    
+    %xlim([-max(mus),max(mus)]); 
+    ylim([0,1]);
+    axis square;
+    plot(mus,squeeze(prop_cw(fci,1,:))', 'color', [0.5,0.5,1], 'linewidth', 1); 
+    plot(mus,squeeze(prop_ccw(fci,1,:))','color', [1,0.5,0.5],'linewidth', 1);  
+    plot(mus,squeeze(prop_cw(fci,2,:))', 'b-', 'linewidth', 1.5); 
+    plot(mus,squeeze(prop_ccw(fci,2,:))','r-', 'linewidth', 1.5); 
+    plot(mus,predPF_ddm{fci,1}(1,:),'bo-','LineWidth',1.5)
+    plot(mus,predPF_ddm{fci,1}(2,:),'ro-','LineWidth',1.5)
+    plot(mus,predPF_ddm{fci,2}(1,:),'color', [0.5,0.5,1],'LineWidth',1.5,'marker','o')
+    plot(mus,predPF_ddm{fci,2}(2,:),'color', [1,0.5,0.5],'LineWidth',1.5,'marker','o')           
+    xlabel('Orientation');
+    ylabel('p(cw)')
+    %xlim([-max(mus),max(mus)]); 
+    ylim([0,1]);
+    axis square;
+    drawnow;  
+end
+% ==> print figure to svg
+print(fig5,[FIGDR,'SI-PFS_AND_PROPS', '-', P_FLAG, '-', M_FLAG, '-', N_FLAG, '-SD-', num2str(sd),'.svg'],'-dsvg')
+
 
 % ==> store averages of DVs at specific time windows
 mu_early_csens_cw_ds = nan(length(fcs),1);
@@ -128,7 +174,7 @@ end
 % ==> plot range
 rg = floor(min([mu_early_csens_cw_ds; mu_early_csens_ccw_ds; mu_late_csens_cw_ds; mu_late_csens_ccw_ds])):ceil(max([mu_early_csens_cw_ds; mu_early_csens_ccw_ds; mu_late_csens_cw_ds; mu_late_csens_ccw_ds]));
 
-figure(10); 
+figure(6); 
 set(gcf,'color','white');
 hold on; hold all;
 scatter(mu_early_csens_cw_ds,mu_late_csens_cw_ds,   100, 'MarkerEdgeColor', 'w', 'MarkerFaceColor', 'r');
@@ -138,9 +184,16 @@ plot(rg,zeros(length(rg)), 'k--');
 plot(zeros(length(rg)),rg, 'k--');
 xlabel('Early DV value (a.u)');
 ylabel('Late DV value');
-title('Early bias + drift');
+if strcmp(P_FLAG,'IMPULSE_PRIOR')
+    title('Early bias + no drift');
+elseif strcmp(P_FLAG, 'REG_DRIFT')
+    title('Early bias + drift')
+end
 axis square;
 drawnow;
+% ==> print figure to svg
+print(gcf,[FIGDR,'F3F-SCATTER', '-', P_FLAG, '-', M_FLAG, '-', N_FLAG, '-SD-', num2str(sd),'.svg'],'-dsvg')
+
 
 
 % ==> correlation between simulated delta bias and simulated delta
@@ -148,12 +201,14 @@ drawnow;
 [r,p] = corr(db',dp');
 
 % ==> plot db and dp
-figure(6); set(gcf,'color','white');
+figure(7); set(gcf,'color','white');
 scatter(db,dp,80,'ko','filled');
 axis square;
 xlabel('Simulated \Delta bias');
 ylabel('Simulated \Delta perceptual uncertainty')
 title(['r = ',num2str(r),', p = ',num2str(p)]);
+% ==> print figure to svg
+print(gcf,[FIGDR,'F5A-SCATTER', '-', P_FLAG, '-', M_FLAG, '-', N_FLAG, '-SD-', num2str(sd),'.svg'],'-dsvg')
 
 
 % ==> Single congruent and incongruent DV
@@ -161,7 +216,7 @@ title(['r = ',num2str(r),', p = ',num2str(p)]);
 ni = randi(size(dvs_i_ccw,1));
 nc = randi(size(dvs_c_ccw,1)); 
 % ==> draw figure
-figure(7); set(gcf,'color','white');
+figure(8); set(gcf,'color','white');
 hold on; hold all;
 plot(t,dvs_i_ccw(ni,:)','linewidth',1.5, 'color', [0.6,0.6,0.6]);
 plot(t,-bnd*ones(1,length(t)),'k--');
@@ -185,8 +240,11 @@ dynr_i_ccw = dynf(dvs_i_ccw);
 
 % ==> plot distribution of dynamic ranges
 dynrs = [dynr_c_cw; dynr_i_cw; dynr_i_ccw; dynr_c_ccw];
-figure(8); set(gcf,'color','white');
+figure(9); set(gcf,'color','white');
 histogram(dynrs,15,'facecolor','w');
 xlabel('Dynamic range (a.u)');
 ylabel('Frequency');
 title('Dynamic range distribution');
+% ==> print figure to svg
+print(gcf,[FIGDR,'F4C-DYN_RANGE_HIST', '-', P_FLAG, '-', M_FLAG, '-', N_FLAG, '-SD-', num2str(sd),'.svg'],'-dsvg')
+
